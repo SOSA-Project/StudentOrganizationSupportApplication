7dec2583e147e1ebae73eceeb0bb663a93896bc0
Gabriel Komor <gabkomti@gmail.com>
2025-10-22 19:58:14 +0200

    SOSA-26 ps1 and code format update

--- app/frontend/main_window.py
+++ app/frontend/main_window.py
@@ -1,6 +1,7 @@
 """
 This file contains main window script
 """
+
 from PIL import Image
 
 import customtkinter as ctk
@@ -18,18 +19,36 @@ class GridMaker:
 
 class IconsHolder:
     def __init__(self):
-        self.calendar_icon = ctk.CTkImage(light_image=Image.open("./app/assets/calendar.png"),
-                                          dark_image=Image.open("./app/assets/calendar.png"), size=(20, 20))
-        self.notification_icon = ctk.CTkImage(light_image=Image.open("./app/assets/bell.png"),
-                                              dark_image=Image.open("./app/assets/bell.png"), size=(20, 20))
-        self.notes_icon = ctk.CTkImage(light_image=Image.open("./app/assets/notes.png"),
-                                       dark_image=Image.open("./app/assets/notes.png"), size=(20, 20))
-        self.grades_icon = ctk.CTkImage(light_image=Image.open("./app/assets/grades.png"),
-                                        dark_image=Image.open("./app/assets/grades.png"), size=(20, 20))
-        self.average_icon = ctk.CTkImage(light_image=Image.open("./app/assets/chart.png"),
-                                         dark_image=Image.open("./app/assets/chart.png"), size=(20, 20))
-        self.settings_icon = ctk.CTkImage(light_image=Image.open("./app/assets/settings.png"),
-                                          dark_image=Image.open("./app/assets/settings.png"), size=(20, 20))
+        self.calendar_icon = ctk.CTkImage(
+            light_image=Image.open("./app/assets/calendar.png"),
+            dark_image=Image.open("./app/assets/calendar.png"),
+            size=(20, 20),
+        )
+        self.notification_icon = ctk.CTkImage(
+            light_image=Image.open("./app/assets/bell.png"),
+            dark_image=Image.open("./app/assets/bell.png"),
+            size=(20, 20),
+        )
+        self.notes_icon = ctk.CTkImage(
+            light_image=Image.open("./app/assets/notes.png"),
+            dark_image=Image.open("./app/assets/notes.png"),
+            size=(20, 20),
+        )
+        self.grades_icon = ctk.CTkImage(
+            light_image=Image.open("./app/assets/grades.png"),
+            dark_image=Image.open("./app/assets/grades.png"),
+            size=(20, 20),
+        )
+        self.average_icon = ctk.CTkImage(
+            light_image=Image.open("./app/assets/chart.png"),
+            dark_image=Image.open("./app/assets/chart.png"),
+            size=(20, 20),
+        )
+        self.settings_icon = ctk.CTkImage(
+            light_image=Image.open("./app/assets/settings.png"),
+            dark_image=Image.open("./app/assets/settings.png"),
+            size=(20, 20),
+        )
 
 
 class ButtonsCreator:
@@ -39,23 +58,47 @@ class ButtonsCreator:
         self.create_buttons()
 
     def create_buttons(self):
-        btn_calendar = ctk.CTkButton(self.parent, text="Calendar", font=("Roboto", 18), image=self.icons.calendar_icon)
+        btn_calendar = ctk.CTkButton(
+            self.parent,
+            text="Calendar",
+            font=("Roboto", 18),
+            image=self.icons.calendar_icon,
+        )
         btn_calendar.grid(row=6, rowspan=3, column=0, columnspan=1, sticky="nsew", padx=8, pady=6)
 
-        btn_notifications = ctk.CTkButton(self.parent, text="Notifications", font=("Roboto", 18),
-                                          image=self.icons.notification_icon)
+        btn_notifications = ctk.CTkButton(
+            self.parent,
+            text="Notifications",
+            font=("Roboto", 18),
+            image=self.icons.notification_icon,
+        )
         btn_notifications.grid(row=9, rowspan=3, column=0, columnspan=1, sticky="nsew", padx=8, pady=6)
 
         btn_notes = ctk.CTkButton(self.parent, text="Notes", font=("Roboto", 18), image=self.icons.notes_icon)
         btn_notes.grid(row=12, rowspan=3, column=0, columnspan=1, sticky="nsew", padx=8, pady=6)
 
-        btn_grades = ctk.CTkButton(self.parent, text="Grades", font=("Roboto", 18), image=self.icons.grades_icon)
+        btn_grades = ctk.CTkButton(
+            self.parent,
+            text="Grades",
+            font=("Roboto", 18),
+            image=self.icons.grades_icon,
+        )
         btn_grades.grid(row=15, rowspan=3, column=0, columnspan=1, sticky="nsew", padx=8, pady=6)
 
-        btn_average = ctk.CTkButton(self.parent, text="Average", font=("Roboto", 18), image=self.icons.average_icon)
+        btn_average = ctk.CTkButton(
+            self.parent,
+            text="Average",
+            font=("Roboto", 18),
+            image=self.icons.average_icon,
+        )
         btn_average.grid(row=18, rowspan=3, column=0, columnspan=1, sticky="nsew", padx=8, pady=6)
 
-        btn_settings = ctk.CTkButton(self.parent, text="Settings", font=("Roboto", 18), image=self.icons.settings_icon)
+        btn_settings = ctk.CTkButton(
+            self.parent,
+            text="Settings",
+            font=("Roboto", 18),
+            image=self.icons.settings_icon,
+        )
         btn_settings.grid(row=29, rowspan=3, column=0, columnspan=1, sticky="nsew", padx=8, pady=6)
 
 
