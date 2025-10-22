f8b6dc735ec7aab5c108963059be2f7b5ce09c47
Gabriel Komor <75366830+gabrielkomor@users.noreply.github.com>
2025-10-22 20:48:37 +0200

    Feature/sosa 26 create first gui implementation (#12)
    * SOSA-26 create main window file and update requirements
    
    * SOSA-26 first gui implementation
    
    * SOSA-26 add buttons into gui
    
    * SOSA-26 add first implementation of left frame for buttons
    
    * SOSA-26 update left frame app
    
    * SOSA-26 add right frame
    
    * SOSA-26 add buttons icons
    
    * SOSA-26 ps1 and code format update
    
    * SOSA-26 add code documentation and type hints
    
    * SOSA-26 add class comment

--- code_check.ps1
+++ code_check.ps1
@@ -1,23 +1,24 @@
 # Venv activation
 $venv = "$PSScriptRoot\venv\Scripts\Activate.ps1"
 if (Test-Path $venv) {
-    Write-Host "Activation virtualenv..."
+    Write-Host "Activating virtualenv..."
     & $venv
 } else {
-    Write-Host "Not found virtualenv!"
+    Write-Host "Virtualenv not found!"
     exit 1
 }
 
 # Mypy
 Write-Host "`n===== Mypy ====="
-python -m mypy "$PSScriptRoot\app"
+python -m mypy "$PSScriptRoot\app" --ignore-missing-imports
 
 # Black
 Write-Host "`n===== Black ====="
-python -m black "$PSScriptRoot\app"
+python -m black --line-length 120 "$PSScriptRoot\app"
 
 # Flake8
 Write-Host "`n===== Flake8 ====="
-python -m flake8 "$PSScriptRoot\app"
+python -m flake8 "$PSScriptRoot\app" --max-line-length 120
 
 Write-Host "`nAll done!"
+
