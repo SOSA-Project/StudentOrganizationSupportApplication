fec24c75baa186bd0d1615a8f226ffebbc3c6ea9
Artur Krol <artur100.krol@gmail.com>
2025-10-23 10:26:04 +0200

    SOSA-22 tweaks

--- app/backend/charts.py
+++ app/backend/charts.py
@@ -2,22 +2,22 @@
 This file contains functions that creates plots
 """
 
+from typing import Callable
 import matplotlib.pyplot as plt
 from collections import Counter
 
 
-colors = ["red", "darkgreen", "green", "forestgreen", "limegreen", "lime"]#Colors of plots
+colors = ["red", "darkgreen", "green", "forestgreen", "limegreen", "lime"]
 
 
-# HISTOGRAM
 def histogram_plot(grades: list[float], theme: str) -> None:
     """
-    Method that creates a histogram plot
+    Function that creates a histogram plot
     :param grades: list of grades
     :param theme: theme as string
-    :return: nothing
+    :return: nothing, only creates a histogram plot
     """
-    plt.rcParams['axes.facecolor'] = 'white'
+    plt.rcParams["axes.facecolor"] = "white"
     grade_counts = Counter(grades)
     unique_sorted = sorted(grade_counts.keys())
     labels = [str(g) for g in unique_sorted]
@@ -25,24 +25,23 @@ def histogram_plot(grades: list[float], theme: str) -> None:
     x = range(len(labels))
 
     fig = plt.figure(figsize=(10, 6), frameon=False)
-    color = 'black'
+    color = "black"
 
     if theme == "light":
         t_color = "black"
-
-    if theme == "dark":
+    elif theme == "dark":
         t_color = "white"
         color = "white"
-        plt.rcParams['axes.facecolor'] = '#242424'
+        plt.rcParams["axes.facecolor"] = "#242424"
 
     ax = fig.add_subplot(1, 1, 1)
-    ax.tick_params(axis='x', colors=color)
+    ax.tick_params(axis="x", colors=color)
     ax.xaxis.label.set_color(color)
     ax.title.set_color(color)
-    ax.spines['bottom'].set_color(color)
-    ax.spines['top'].set_color(color)
-    ax.spines['left'].set_color(color)
-    ax.spines['right'].set_color(color)
+    ax.spines["bottom"].set_color(color)
+    ax.spines["top"].set_color(color)
+    ax.spines["left"].set_color(color)
+    ax.spines["right"].set_color(color)
     plt.bar(x, heights, color=colors, width=0.5, edgecolor=color)
 
     plt.xticks(x, labels)
@@ -56,32 +55,30 @@ def histogram_plot(grades: list[float], theme: str) -> None:
     plt.show()
 
 
-# PIE CHART
 def pie_plot(grades: list[float], theme: str) -> None:
     """
-    Method that creates a pie plot
+    Function that creates a pie plot
     :param grades: list of grades
     :param theme: theme as string
-    :return:
+    :return: nothing, only creates a pie plot
     """
     grade_counts = Counter(grades)
     values = list(grade_counts.values())
 
     if theme == "light":
-        color = 'black'
+        color = "black"
+    elif theme == "dark":
+        color = "white"
 
-    if theme == "dark":
-        color = 'white'
-
-    def make_autopct(values):
-        def my_autopct(pct):
+    def make_autopct(values: list[int]) -> Callable[[float], str]:
+        def my_autopct(pct: float) -> str:
             total = sum(values)
             count = int(round(pct * total / 100.0))
             return f"{count}"
 
         return my_autopct
 
-    plt.figure(figsize=(6, 6),  frameon=False)
+    plt.figure(figsize=(6, 6), frameon=False)
     plt.pie(
         grade_counts.values(),
         labels=[str(k) for k in grade_counts.keys()],
