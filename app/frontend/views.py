4c26115e13d5c48bbf595f5293c6391d8bf0f5cb
Gabriel Komor <gabkomti@gmail.com>
2025-10-26 21:41:47 +0100

    SOSA-40 add code comments for view file

--- app/frontend/views.py
+++ app/frontend/views.py
@@ -1,9 +1,18 @@
+"""
+This file contains views for all widgets.
+"""
+
 from abc import ABC, abstractmethod
 
 import customtkinter as ctk
 
+
 class BaseView(ctk.CTkFrame, ABC):
-    def __init__(self, parent):
+    """
+    This class is a template for the remaining views.
+    """
+
+    def __init__(self, parent) -> None:
         super().__init__(parent, fg_color="#444444", corner_radius=10)
         [self.grid_rowconfigure(index=i, weight=1, uniform="rowcol") for i in range(32)]
         [self.grid_columnconfigure(index=i, weight=1, uniform="rowcol") for i in range(1)]
@@ -11,65 +20,109 @@ class BaseView(ctk.CTkFrame, ABC):
         self.update_idletasks()
 
     @abstractmethod
-    def create_frame_content(self):
+    def create_frame_content(self) -> None:
         pass
 
 
 class CalendarView(BaseView):
-    def __init__(self, parent):
+    """
+    View for calendar widget.
+    """
+
+    def __init__(self, parent) -> None:
         super().__init__(parent)
         self.create_frame_content()
 
-    def create_frame_content(self):
+    def create_frame_content(self) -> ctk.CTkFrame:
+        """
+        This method creates elements visible on the frame.
+        :return: new ctk frame.
+        """
         label_one = ctk.CTkLabel(self, text="Calendar", font=("Roboto", 18))
         label_one.grid(row=0, rowspan=2, column=0, columnspan=1, padx=5, pady=5)
 
 
 class NotificationsView(BaseView):
-    def __init__(self, parent):
+    """
+    View for notifications widget.
+    """
+
+    def __init__(self, parent) -> None:
         super().__init__(parent)
         self.create_frame_content()
 
-    def create_frame_content(self):
+    def create_frame_content(self) -> ctk.CTkFrame:
+        """
+        This method creates elements visible on the frame.
+        :return: new ctk frame.
+        """
         label_one = ctk.CTkLabel(self, text="Notifications", font=("Roboto", 18))
         label_one.grid(row=0, rowspan=2, column=0, columnspan=1, padx=5, pady=5)
 
 
 class NotesView(BaseView):
-    def __init__(self, parent):
+    """
+    View for notes widget.
+    """
+
+    def __init__(self, parent) -> None:
         super().__init__(parent)
         self.create_frame_content()
 
-    def create_frame_content(self):
+    def create_frame_content(self) -> ctk.CTkFrame:
         label_one = ctk.CTkLabel(self, text="Notes", font=("Roboto", 18))
         label_one.grid(row=0, rowspan=2, column=0, columnspan=1, padx=5, pady=5)
 
 
 class GradesView(BaseView):
-    def __init__(self, parent):
+    """
+    View for grades widget.
+    """
+
+    def __init__(self, parent) -> None:
         super().__init__(parent)
         self.create_frame_content()
 
-    def create_frame_content(self):
+    def create_frame_content(self) -> ctk.CTkFrame:
+        """
+        This method creates elements visible on the frame.
+        :return: new ctk frame.
+        """
         label_one = ctk.CTkLabel(self, text="Grades", font=("Roboto", 18))
         label_one.grid(row=0, rowspan=2, column=0, columnspan=1, padx=5, pady=5)
 
 
 class AverageView(BaseView):
-    def __init__(self, parent):
+    """
+    View for average widget.
+    """
+
+    def __init__(self, parent) -> None:
         super().__init__(parent)
         self.create_frame_content()
 
-    def create_frame_content(self):
+    def create_frame_content(self) -> ctk.CTkFrame:
+        """
+        This method creates elements visible on the frame.
+        :return: new ctk frame.
+        """
         label_one = ctk.CTkLabel(self, text="Average", font=("Roboto", 18))
         label_one.grid(row=0, rowspan=2, column=0, columnspan=1, padx=5, pady=5)
 
 
 class SettingsView(BaseView):
-    def __init__(self, parent):
+    """
+    View for settings widget.
+    """
+
+    def __init__(self, parent) -> None:
         super().__init__(parent)
         self.create_frame_content()
 
-    def create_frame_content(self):
+    def create_frame_content(self) -> ctk.CTkFrame:
+        """
+        This method creates elements visible on the frame.
+        :return: new ctk frame.
+        """
         label_one = ctk.CTkLabel(self, text="Settings", font=("Roboto", 18))
         label_one.grid(row=0, rowspan=2, column=0, columnspan=1, padx=5, pady=5)
