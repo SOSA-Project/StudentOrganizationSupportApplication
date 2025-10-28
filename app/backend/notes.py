6bd79e141e3bc0f4c97cf76ee13a22b02c724dd2
MichałTokarczyk <mictok25@interia.pl>
2025-10-28 22:40:17 +0100

    SOSA-35 created module for notes and added update methods of attributes

--- app/backend/notes.py
+++ app/backend/notes.py
@@ -16,3 +16,27 @@ class Note:
         self.title = title
         self.content = content
         self.created_at = str(datetime.now().strftime("%Y-%m-%d %H:%M"))
+
+    def update_title(self, new_title: str) -> None:
+        """
+        Updates the title of the note
+        :param new_title: new title of the note
+        :return: Nothing
+        """
+        self.title = new_title
+
+    def update_content(self, new_content: str) -> None:
+        """
+        Updates the content of the note
+        :param new_content: new content of the note
+        :return: Nothing
+        """
+        self.content = new_content
+
+    def update_user_id(self, new_user_id: int) -> None:
+        """
+        Updates the user_id of the note
+        :param new_user_id: new user_id of the note
+        :return: Nothing
+        """
+        self.user_id = new_user_id
