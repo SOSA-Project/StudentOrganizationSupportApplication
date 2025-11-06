cfe302785a749ded495a7fe7067d4d6d96edf522
Grzegorz Ryński <139359004+GrzegorzRynski@users.noreply.github.com>
2025-11-06 12:22:39 +0100

    Feature/sosa 44 check possible calendar implementations (#32)
    * SOSA-44 Implemented calendar widget prototype
    
    * SOSA-44 Code clean up and added comments

--- app/frontend/views.py
+++ app/frontend/views.py
@@ -2,6 +2,7 @@
 This file contains views for all widgets.
 """
 
+import random
 from abc import ABC, abstractmethod
 
 
@@ -9,6 +10,8 @@ import customtkinter as ctk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+from datetime import datetime
+import calendar
 
 from app.backend.grade_monitor import initiate_grade_monitor
 from app.backend.charts import StatisticsManager, subjects_averages_histogram_plot
@@ -43,6 +46,7 @@ class CalendarView(BaseView):
 
     def __init__(self, parent: ctk.CTk) -> None:
         super().__init__(parent)
+        self.current_date = datetime.now()
         self.create_frame_content()
 
     def create_frame_content(self) -> ctk.CTkFrame:
@@ -50,8 +54,86 @@ class CalendarView(BaseView):
         This method creates elements visible on the frame.
         :return: new ctk frame.
         """
-        label_one: ctk.CTkLabel = ctk.CTkLabel(self, text="Calendar", font=("Roboto", 18))
-        label_one.grid(row=0, rowspan=2, column=0, columnspan=1, padx=5, pady=5)
+        header_frame = ctk.CTkFrame(self)
+        header_frame.pack(pady=10, padx=20, fill="both")
+
+        prev_btn = ctk.CTkButton(header_frame, text="<", width=40, command=self.prev_month)
+        prev_btn.pack(side="left", padx=10)
+
+        self.header = ctk.CTkLabel(header_frame, text="", font=("Roboto", 18))
+        self.header.pack(side="left", expand=True)
+
+        next_btn = ctk.CTkButton(header_frame, text=">", width=40, command=self.next_month)
+        next_btn.pack(side="right", padx=10)
+
+        self.calendar_frame = ctk.CTkFrame(self)
+        self.calendar_frame.pack(pady=10, padx=20, fill="both", expand=True)
+
+        self.update_calendar()
+
+    def update_calendar(self) -> None:
+        """
+        This method updates the calendar view with the according month and a year destroying previous widgets
+        and creating new ones in their place
+        :return: Nothing
+        """
+        for widget in self.calendar_frame.winfo_children():
+            widget.destroy()
+
+        year, month = self.current_date.year, self.current_date.month
+        self.header.configure(text=f"{calendar.month_name[month]} {year}")
+
+        week_days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]
+        for idx, day in enumerate(week_days):
+            (
+                ctk.CTkLabel(self.calendar_frame, text=day, font=("Roboto", 20, "bold")).grid(
+                    row=0, column=idx, padx=5, pady=5
+                )
+            )
+
+        cal = calendar.monthcalendar(year, month)
+        for r, w in enumerate(cal, start=1):
+            for c, d in enumerate(w):
+                if d == 0:
+                    continue
+                btn = ctk.CTkButton(self.calendar_frame, text=str(d), width=40, height=30, font=("Roboto", 18, "bold"))
+                btn.configure(command=lambda b=btn: self.placeholder_action(b))
+                btn.grid(row=r, column=c, padx=3, pady=3, sticky="nsew")
+
+        for col in range(7):
+            self.calendar_frame.grid_columnconfigure(col, weight=1)
+        for row in range(len(cal) + 1):
+            self.calendar_frame.grid_rowconfigure(row, weight=1)
+
+    def placeholder_action(self, btn: ctk.CTkButton) -> None:
+        """
+        This method is just a placeholder and will be removed in the future
+        :param btn: Day button placeholder
+        :return: Nothing
+        """
+        btn.configure(fg_color="#" + str(random.randint(100000, 999999)))
+
+    def prev_month(self) -> None:
+        """
+        Changes currently viewed month to a previous month and updates the calendar view
+        :return: Nothing
+        """
+        if self.current_date.month == 1:
+            self.current_date = self.current_date.replace(year=self.current_date.year - 1, month=12)
+        else:
+            self.current_date = self.current_date.replace(month=self.current_date.month - 1)
+        self.update_calendar()
+
+    def next_month(self) -> None:
+        """
+        Changes currently viewed month to a following month and updates the calendar view
+        :return: Nothing
+        """
+        if self.current_date.month == 12:
+            self.current_date = self.current_date.replace(year=self.current_date.year + 1, month=1)
+        else:
+            self.current_date = self.current_date.replace(month=self.current_date.month + 1)
+        self.update_calendar()
 
 
 class NotificationsView(BaseView):
