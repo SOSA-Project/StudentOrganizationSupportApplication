2a1370b047e73fa1018265dbad388d8dbe349288
Krulu <artur100.krol@gmail.com>
2025-11-13 11:39:45 +0100

    Feature/sosa 51 create backend for notes (#43)
    * SOSA-51 implementation of NoteManager and GUI integration
    
    * SOSA-51 code tweaks and fixed notes sharp corner issue
    
    * SOSA-51 tweaks done by code_check

--- app/backend/notes.py
+++ app/backend/notes.py
@@ -1,8 +1,10 @@
 """
-File contains definition of Note class
+File contains definition of Note and NoteManager class
 """
 
 from datetime import datetime
+
+from app.backend.data_base import fetch_notes
 
 
 class Note:
@@ -40,3 +42,58 @@ class Note:
         :return: Nothing
         """
         self.user_id = new_user_id
+
+
+class NoteManager:
+    """
+    Class responsible for managing notes fetched from the database.
+    """
+
+    def __init__(self, notes_list: list[tuple[int, str, str, str, int]]) -> None:
+        self.notes: list[Note] = []
+        self.fill_notes_table(notes_list)
+
+    def fill_notes_table(self, notes_list: list[tuple[int, str, str, str, int]]) -> None:
+        """
+        Method that fills notes table with data fetched from the database.
+        :param notes_list: List of tuples representing notes
+        :return: Nothing
+        """
+        for row in notes_list:
+            note_id, title, content, created_at, user_id = row
+            note = Note(note_id, user_id, title, content)
+            note.created_at = created_at
+            self.notes.append(note)
+
+    def get_all_notes(self) -> list[Note]:
+        """
+        Method that returns all notes.
+        :return: List of Note objects
+        """
+        return self.notes
+
+
+def initiate_note_manager() -> NoteManager | None:
+    """
+    Function that fetches data from the database, validates it, and returns a NoteManager instance.
+    :return: An instance of NoteManager class or None
+    """
+    try:
+        notes = fetch_notes()
+        if not notes or not isinstance(notes, list):
+            return None
+
+        def valid_item(item: tuple) -> bool:
+            expected_types = (int, str, str, str, int)
+            if not isinstance(item, tuple) or len(item) != len(expected_types):
+                return False
+            return all(isinstance(x, t) for x, t in zip(item, expected_types))
+
+        if all(valid_item(item) for item in notes):
+            return NoteManager(notes)
+
+        return None
+
+    except TypeError:
+        print("Notes could not be fetched from database")
+        return None
